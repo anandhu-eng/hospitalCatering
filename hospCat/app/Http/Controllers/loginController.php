b50928a42c072b624953bf51f19ef60dc2f14835
ANANDHU S <anandhukicks@gmail.com>
2023-11-21 21:46:12 +0530

    fixes

--- hospCat/app/Http/Controllers/loginController.php
+++ hospCat/app/Http/Controllers/loginController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PatientDetail;
+use App\Models\CurrentPatient;
+use App\Models\Foods;
 
 class loginController extends Controller
 {
     public function checkNo(Request $request){
         // Get the phone number from the request
         $phoneNumber = $request->input('phone');
-        // verofy whether the user is admitted or not
+        // verify whether the user is admitted or not
+        $patient = PatientDetail::where('PhNo', $phoneNumber)->first();
+        if($patient){
+            $currentPatient = CurrentPatient::where('PID',$patient->PID)->first();
+            if ($currentPatient){
+                //contents  to be displayed in home page
+                // patient details(PID only), food items
 
+                $patient_details = $currentPatient->PID;
+                $food_records = Foods::all();
+
+                // return $food_records;    
+                // return $patient_details;
+                return view('home', ['patient_details' => $patient_details,
+                'food_records' => $food_records]);
+
+            }
+        }
         //send otp to the user if approved
-        
         return $phoneNumber;
+        
     }
 }
+
