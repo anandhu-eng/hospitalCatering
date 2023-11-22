ede943c523f4f7a28cb409bf7f1cd6381d81ed53
ANANDHU S <anandhukicks@gmail.com>
2023-11-22 06:36:11 +0530

    profile page integrated

--- hospCat/app/Http/Controllers/profileController.php
+++ hospCat/app/Http/Controllers/profileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\PatientDetail;
+use App\Models\CurrentPatient;
 
 class profileController extends Controller
 {
@@ -11,6 +12,7 @@ class profileController extends Controller
         // get the pid from url
         $pid = $request->input('pid');
         $patientDetails = PatientDetail::where('PID', $pid)->first();
-        return response()->json($patientDetails);
+        $wardNo = currentPatient::where('PID', $pid)->first();
+        return view('patientProfile', ['patientDetails' => $patientDetails, 'wardDetails' => $wardNo]);
     }
 }
